8129201bd3eaeaf8e4e0018d6986069118f9b768
naveen-1711-devoloper <mumminanaveen332@gmail.com>
2026-01-30 10:26:17 +0530

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -8,4 +8,4 @@ apt install -y gcc g++ make gdb
 apt install -y python3 python3-pip python3-venv
 apt install -y openjdk-17-jdk
 apt install -y git vim curl wget tree net-tools
-apt install -y vlc-bin
+apt install -y vlc
