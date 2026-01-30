3c9be913f4e1bf4e4163114455bd3d91e4eecde3
naveen-1711-devoloper <mumminanaveen332@gmail.com>
2026-01-30 10:41:52 +0530

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -4,8 +4,4 @@ set -e
 
 apt update -y
 
-apt install -y gcc g++ make gdb
-apt install -y python3 python3-pip python3-venv
-apt install -y openjdk-17-jdk
-apt install -y git vim curl wget tree net-tools
-apt install -y vlc
+apt install -y gcc g++ make gdb python3 python3-pip python3-venv openjdk-17-jdk git vim curl wget tree net-tools vlc
